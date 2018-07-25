0df4105c6266b993b918eae0eb011a0de07bc928
Edit Xie <34528608+EditXie@users.noreply.github.com>
2018-07-25 17:06:48 +0800

    切换显示标签
    切换显示标签

--- js/script.js
+++ js/script.js
@@ -44,4 +44,17 @@ $(document).ready(() => {
 		` )
 	})
 	
+	//  监听触发按钮方法与事件
+	$('#dropdown-demo').on('show.bs.dropdown', () => {
+		console.log('show')
+	})
+	$('#dropdown-demo').on('shown.bs.dropdown', () => {
+		console.log('shown')
+	})
+	$('#dropdown-demo').on('hide.bs.dropdown', () => {
+		console.log('hide')
+	})
+	$('#dropdown-demo').on('hidden.bs.dropdown', () => {
+		console.log('hidden')
+	})
 })
